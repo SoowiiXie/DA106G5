067b0f8c6d2adf7f93eae6be4a3873678b66ce91
Xuan <wpmfm@hotmail.com>
2020-04-01 15:40:23 +0800

    新增會員假照片 by 名軒

--- src/com/common/ImportImages.java
+++ src/com/common/ImportImages.java
@@ -2,43 +2,39 @@ package com.common;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import com.xuan.mb.model.*;
 
 public class ImportImages {
 
 	public static void main(String[] args) {
-		String[] mb_id = {"soowii123","xuan123","michael123","vain123","yiwen123"};
-		String[] pics = {"1","2","3","4","5"};
+		String[] mb_id = {"soowii123","xuan123","michael123","vain123","yiwen123","weijhih123"};
 		
 		Connection con = null;
 		PreparedStatement pstmt = null;
+		InputStream fin = null;
+		int count = 0;
 
 		try {
 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "DA106G5", "DA106G5");
-			pstmt = con.prepareStatement("UPDATE MEMBER set MB_PIC=? where MB_ID = ?");
+			pstmt = con.prepareStatement("UPDATE MEMBER SET MB_PIC=? WHERE MB_ID = ?");
 			
-			for(int i = 0; i < mb_id.length; i++) {
+			for(int i = 0; i < 6; i++) {
 				
-				File pic = new File("images", pics[i] + ".jpg");
+				fin = new FileInputStream(new File("fake_picture", "mb" + (i+1) + ".jpg")); 
 				
-				byte[] picb = new byte[(int)pic.length()];
+				pstmt.setBinaryStream(1, fin, fin.available());
+				pstmt.setString(2, mb_id[i]);
+				count += pstmt.executeUpdate();
 				
-				InputStream fin = new FileInputStream(pic); 
-				
-				
-				pstmt.setBytes(1, MemberVO.getMb_pic());
-				pstmt.setString(2, MemberVO.getMb_id());
-
-				pstmt.executeUpdate();
 			}
-			
+			System.out.println(count + " images inserted!!");
 
 			// Handle any driver errors
 		} catch (ClassNotFoundException e) {
@@ -48,8 +44,17 @@ public class ImportImages {
 		} catch (SQLException se) {
 			throw new RuntimeException("A database error occured. "
 					+ se.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
 			// Clean up JDBC resources
 		} finally {
+			if(fin != null) {
+				try {
+					fin.close();
+				} catch (IOException e) {
+					e.printStackTrace(System.err);
+				}
+			}
 			if (pstmt != null) {
 				try {
 					pstmt.close();
