eb0dbf8ca3723fb738c6670f08ce7d09551ea969
soowii <e64009117@gs.ncku.edu.tw>
2020-04-02 01:18:50 +0800

    把test拉出來做serverlet以測試連線池 by戍乂

--- src/com/location/model/LocationDAO.java
+++ src/com/location/model/LocationDAO.java
@@ -2,10 +2,6 @@ package com.location.model;
 
 import static com.common.Common.*;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -24,7 +20,7 @@ public class LocationDAO implements Location_interface {
 //	String userid = "DA106G5";
 //	String passwd = "DA106G5";
 
-	private static final String INSERT_STMT = "INSERT INTO Location (loc_no, loc_typeno, longitude, latitude, loc_status, loc_address, loc_pic) VALUES (?, ?, ?, ?, ?, ?, ?)";
+	private static final String INSERT_STMT = "INSERT INTO Location (loc_no, loc_typeno, longitude, latitude, loc_status, loc_address, loc_pic) VALUES ('loc'||LPAD(to_char(loc_no_seq.NEXTVAL), 5, '0'),?,?,?,?,?,?)";
 	private static final String UPDATE = "UPDATE Location set loc_typeno=?, longitude=?, latitude=?, loc_status=?, loc_address=?, loc_pic=? where loc_no = ?";
 	private static final String DELETE = "DELETE from Location where loc_no = ?";
 	private static final String GET_ONE_STMT = "SELECT * FROM Location where loc_no = ?";
@@ -32,41 +28,47 @@ public class LocationDAO implements Location_interface {
 	private static final String GET_BY_LOC_TYPENO = "SELECT * FROM Location where loc_typeno = ? order by loc_no";
 
 	// 一個應用程式中,針對一個資料庫 ,共用一個DataSource即可
-		private static DataSource ds = null;
-		static {
-			try {
-				Context ctx = new InitialContext();
-				ds = (DataSource) ctx.lookup("java:comp/env/jdbc/TestDB");
+	private static DataSource ds = null;
+	static {
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/TestDB2");
 		} catch (NamingException e) {
-				e.printStackTrace();
-			}
+			e.printStackTrace();
 		}
+	}
 
-	
 	@Override
 	public void insert(LocationVO locationVO) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
+		String next_loc_no = null;
 
 		try {
+//			Class.forName(DRIVER_CLASS);
+//			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			con = ds.getConnection();
+			String cols[] = { "loc_no" };
+			pstmt = con.prepareStatement(INSERT_STMT, cols);
 
-			Class.forName(DRIVER_CLASS);
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
-			pstmt = con.prepareStatement(INSERT_STMT);
-
-			pstmt.setString(1, locationVO.getLoc_no());
-			pstmt.setString(2, locationVO.getLoc_typeno());
-			pstmt.setString(3, locationVO.getLongitude());
-			pstmt.setString(4, locationVO.getLatitude());
-			pstmt.setInt(5, locationVO.getLoc_status());
-			pstmt.setString(6, locationVO.getLoc_address());
-			pstmt.setBytes(7, locationVO.getLoc_pic());
+//			pstmt.setString(1, locationVO.getLoc_no());
+			pstmt.setString(1, locationVO.getLoc_typeno());
+			pstmt.setString(2, locationVO.getLongitude());
+			pstmt.setString(3, locationVO.getLatitude());
+			pstmt.setInt(4, locationVO.getLoc_status());
+			pstmt.setString(5, locationVO.getLoc_address());
+			pstmt.setBytes(6, locationVO.getLoc_pic());
 
 			pstmt.executeUpdate();
+			// 取得對應的自增主鍵值
+			ResultSet rs = pstmt.getGeneratedKeys();
+			if (rs.next()) {
+				next_loc_no = rs.getString(1);
+				System.out.println("自增主鍵值= " + next_loc_no + "(剛新增成功的編號)");
+			} else {
+				System.out.println("未取得自增主鍵值");
+			}
 			// Handle any driver errors
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
-			// Handle any SQL errors
 		} catch (SQLException se) {
 			throw new RuntimeException("A database error occured. " + se.getMessage());
 			// Clean up JDBC resources
@@ -95,9 +97,9 @@ public class LocationDAO implements Location_interface {
 		PreparedStatement pstmt = null;
 
 		try {
-
-			Class.forName(DRIVER_CLASS);
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
+//			Class.forName(DRIVER_CLASS);
+//			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			con = ds.getConnection();
 			pstmt = con.prepareStatement(UPDATE);
 
 			pstmt.setString(7, locationVO.getLoc_no());
@@ -110,9 +112,6 @@ public class LocationDAO implements Location_interface {
 
 			pstmt.executeUpdate();
 			// Handle any driver errors
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
-			// Handle any SQL errors
 		} catch (SQLException se) {
 			throw new RuntimeException("A database error occured. " + se.getMessage());
 			// Clean up JDBC resources
@@ -191,9 +190,9 @@ public class LocationDAO implements Location_interface {
 		ResultSet rs = null;
 
 		try {
-
-			Class.forName(DRIVER_CLASS);
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
+//			Class.forName(DRIVER_CLASS);
+//			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			con = ds.getConnection();
 			pstmt = con.prepareStatement(GET_ONE_STMT);
 
 			pstmt.setString(1, loc_no);
@@ -213,9 +212,6 @@ public class LocationDAO implements Location_interface {
 			}
 
 			// Handle any driver errors
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
-			// Handle any SQL errors
 		} catch (SQLException se) {
 			throw new RuntimeException("A database error occured. " + se.getMessage());
 			// Clean up JDBC resources
@@ -255,9 +251,9 @@ public class LocationDAO implements Location_interface {
 		ResultSet rs = null;
 
 		try {
-
-			Class.forName(DRIVER_CLASS);
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
+//			Class.forName(DRIVER_CLASS);
+//			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			con = ds.getConnection();
 			pstmt = con.prepareStatement(GET_ALL_STMT);
 			rs = pstmt.executeQuery();
 
@@ -276,9 +272,6 @@ public class LocationDAO implements Location_interface {
 			}
 
 			// Handle any driver errors
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
-			// Handle any SQL errors
 		} catch (SQLException se) {
 			throw new RuntimeException("A database error occured. " + se.getMessage());
 			// Clean up JDBC resources
@@ -317,9 +310,9 @@ public class LocationDAO implements Location_interface {
 		ResultSet rs = null;
 
 		try {
-
-			Class.forName(DRIVER_CLASS);
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
+//			Class.forName(DRIVER_CLASS);
+//			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			con = ds.getConnection();
 			pstmt = con.prepareStatement(GET_BY_LOC_TYPENO);
 			pstmt.setString(1, loc_typeno);
 
@@ -340,9 +333,6 @@ public class LocationDAO implements Location_interface {
 			}
 
 			// Handle any driver errors
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
-			// Handle any SQL errors
 		} catch (SQLException se) {
 			throw new RuntimeException("A database error occured. " + se.getMessage());
 			// Clean up JDBC resources
@@ -370,96 +360,6 @@ public class LocationDAO implements Location_interface {
 			}
 		}
 		return set;
-	}
-
-	// 測試
-	public static void main(String[] args) {
-		LocationDAO dao = new LocationDAO();
-
-		// 增 loc_no, loc_typeno, longitude, latitude, loc_status, loc_address, loc_pic
-		LocationVO locationVO_insert = new LocationVO();
-		locationVO_insert.setLoc_no("loc00007");
-		locationVO_insert.setLoc_typeno("3");
-		locationVO_insert.setLongitude("123.456");
-		locationVO_insert.setLatitude("78.90");
-		locationVO_insert.setLoc_status(7);// 有給default
-		locationVO_insert.setLoc_address("桃園");
-		FileInputStream in;
-		BufferedInputStream bf;
-		try {
-			in = new FileInputStream("fake_picture/loc" + String.format("%05d", 6) + ".jpg");
-			bf = new BufferedInputStream(in);
-			byte[] image = new byte[bf.available()];// 讀入的圖檔,暫存在記憶體
-			bf.read(image);
-			locationVO_insert.setLoc_pic(image);
-			bf.close();
-			in.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		dao.insert(locationVO_insert);
-
-		// 改
-		LocationVO locationVO_update = new LocationVO();
-		locationVO_update.setLoc_no("loc00007");
-		locationVO_update.setLoc_typeno("3");
-		locationVO_update.setLongitude("123.456");
-		locationVO_update.setLatitude("78.90");
-		locationVO_update.setLoc_status(7);// 有給default
-		locationVO_update.setLoc_address("全家就是我家");
-		try {
-			in = new FileInputStream("fake_picture/loc" + String.format("%05d", 6) + ".jpg");
-			bf = new BufferedInputStream(in);
-			byte[] image = new byte[bf.available()];// 讀入的圖檔,暫存在記憶體
-			bf.read(image);
-			locationVO_update.setLoc_pic(image);
-			bf.close();
-			in.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		dao.update(locationVO_update);
-
-		// 用PK查詢
-		LocationVO locationVO_getByPK = dao.findByPrimaryKey("loc00007");
-		System.out.println("新增並修改的PK是:" + locationVO_getByPK.getLoc_no());
-		System.out.print("修改後的值是:");
-		System.out.print(locationVO_getByPK.getLoc_typeno() + ",");
-		System.out.print(locationVO_getByPK.getLongitude() + ",");
-		System.out.print(locationVO_getByPK.getLatitude() + ",");
-		System.out.print(locationVO_getByPK.getLoc_status() + ",");
-		System.out.print(locationVO_getByPK.getLoc_address() + ",");
-		System.out.println(locationVO_getByPK.getLoc_pic());
-
-		// 刪
-		dao.delete("loc00007");
-
-		// 查
-		List<LocationVO> all = dao.getAll();
-		System.out.println("\n刪掉剛剛新增的後剩:");
-		for (LocationVO locationVO : all) {
-			System.out.println(locationVO.getLoc_no() + "," + locationVO.getLoc_typeno() + ","
-					+ locationVO.getLongitude() + "," + locationVO.getLatitude() + "," + locationVO.getLoc_status()
-					+ "," + locationVO.getLoc_address() + "," + locationVO.getLoc_pic());
-		}
-
-		// 查
-		Set<LocationVO> loc_typenoAll = dao.getLocationByLoc_typeno("1");
-		System.out.println("\n取得所有Loc_typeno=1的地標:");
-		for (LocationVO locationVO : loc_typenoAll) {
-			System.out.println(locationVO.getLoc_no() + "," + locationVO.getLoc_typeno() + ","
-					+ locationVO.getLongitude() + "," + locationVO.getLatitude() + "," + locationVO.getLoc_status()
-					+ "," + locationVO.getLoc_address() + "," + locationVO.getLoc_pic());
-		}
-
 	}
 
 }
---
+++ src/com/location/model/LocationTest.java
@@ -0,0 +1,109 @@
+package com.location.model;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LocationTest extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		LocationDAO dao = new LocationDAO();
+
+		// 增 loc_no, loc_typeno, longitude, latitude, loc_status, loc_address, loc_pic
+		LocationVO locationVO_insert = new LocationVO();
+//		locationVO_insert.setLoc_no("loc00007");
+		locationVO_insert.setLoc_typeno("3");
+		locationVO_insert.setLongitude("123.456");
+		locationVO_insert.setLatitude("78.90");
+		locationVO_insert.setLoc_status(7);// 有給default
+		locationVO_insert.setLoc_address("桃園");
+		FileInputStream in;
+		BufferedInputStream bf;
+		try {
+			in = new FileInputStream("fake_picture/loc" + String.format("%05d", 6) + ".jpg");
+			bf = new BufferedInputStream(in);
+			byte[] image = new byte[bf.available()];// 讀入的圖檔,暫存在記憶體
+			bf.read(image);
+			locationVO_insert.setLoc_pic(image);
+			bf.close();
+			in.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		dao.insert(locationVO_insert);
+
+		// 改
+		LocationVO locationVO_update = new LocationVO();
+		locationVO_update.setLoc_no("loc00007");
+		locationVO_update.setLoc_typeno("3");
+		locationVO_update.setLongitude("123.456");
+		locationVO_update.setLatitude("78.90");
+		locationVO_update.setLoc_status(7);// 有給default
+		locationVO_update.setLoc_address("全家就是我家");
+		try {
+			in = new FileInputStream("fake_picture/loc" + String.format("%05d", 6) + ".jpg");
+			bf = new BufferedInputStream(in);
+			byte[] image = new byte[bf.available()];// 讀入的圖檔,暫存在記憶體
+			bf.read(image);
+			locationVO_update.setLoc_pic(image);
+			bf.close();
+			in.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		dao.update(locationVO_update);
+
+		// 用PK查詢
+		LocationVO locationVO_getByPK = dao.findByPrimaryKey("loc00007");
+		System.out.println("新增並修改的PK是:" + locationVO_getByPK.getLoc_no());
+		System.out.print("修改後的值是:");
+		System.out.print(locationVO_getByPK.getLoc_typeno() + ",");
+		System.out.print(locationVO_getByPK.getLongitude() + ",");
+		System.out.print(locationVO_getByPK.getLatitude() + ",");
+		System.out.print(locationVO_getByPK.getLoc_status() + ",");
+		System.out.print(locationVO_getByPK.getLoc_address() + ",");
+		System.out.println(locationVO_getByPK.getLoc_pic());
+
+		// 刪
+//		dao.delete("loc00007");
+
+		// 查
+		List<LocationVO> all = dao.getAll();
+		System.out.println("\n刪掉剛剛新增的後剩:");
+		for (LocationVO locationVO : all) {
+			System.out.println(locationVO.getLoc_no() + "," + locationVO.getLoc_typeno() + ","
+					+ locationVO.getLongitude() + "," + locationVO.getLatitude() + "," + locationVO.getLoc_status()
+					+ "," + locationVO.getLoc_address() + "," + locationVO.getLoc_pic());
+		}
+
+		// 查
+		Set<LocationVO> loc_typenoAll = dao.getLocationByLoc_typeno("1");
+		System.out.println("\n取得所有Loc_typeno=1的地標:");
+		for (LocationVO locationVO : loc_typenoAll) {
+			System.out.println(locationVO.getLoc_no() + "," + locationVO.getLoc_typeno() + ","
+					+ locationVO.getLongitude() + "," + locationVO.getLatitude() + "," + locationVO.getLoc_status()
+					+ "," + locationVO.getLoc_address() + "," + locationVO.getLoc_pic());
+		}
+	}
+}
