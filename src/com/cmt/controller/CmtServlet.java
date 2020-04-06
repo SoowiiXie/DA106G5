42da812d60f0a387c5000f9daa221c8fc90aab89
soowii <e64009117@gs.ncku.edu.tw>
2020-04-06 23:40:30 +0800

    做完cmt的C by戍乂

--- src/com/cmt/controller/CmtServlet.java
+++ src/com/cmt/controller/CmtServlet.java
@@ -11,7 +11,7 @@ import com.cmt.model.*;
 
 @MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 5 * 100 * 1024 * 1024, maxRequestSize = 5 * 5 * 100
 		* 1024 * 1024)
-public class LocationServlet extends HttpServlet {
+public class CmtServlet extends HttpServlet {
 
 	/**
 	 * 
@@ -134,96 +134,53 @@ public class LocationServlet extends HttpServlet {
 //					errorMsgs.add("員工姓名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間");
 				}
 				java.sql.Date cmt_time = null;
+//				try {
+				cmt_time = java.sql.Date.valueOf(req.getParameter("cmt_time").trim());
+//				} catch (IllegalArgumentException e) {
+//					cmt_time=new java.sql.Date(System.currentTimeMillis());
+//					errorMsgs.add("請輸入日期!");
+//				}
+				Integer cmt_status = null;
 				try {
-					cmt_time = java.sql.Date.valueOf(req.getParameter("cmt_time").trim());
-				} catch (IllegalArgumentException e) {
-					cmt_time=new java.sql.Date(System.currentTimeMillis());
-					errorMsgs.add("請輸入日期!");
-				}
-
-				String longitude = req.getParameter("longitude").trim();
-				if (longitude == null || longitude.trim().length() == 0) {
-					errorMsgs.add("經度請勿空白");
-				}
-
-				String latitude = req.getParameter("latitude").trim();
-				if (latitude == null || latitude.trim().length() == 0) {
-					errorMsgs.add("緯度請勿空白");
-				}
-
-
-
-				Integer loc_status = null;
-				try {
-					loc_status = new Integer(req.getParameter("loc_status").trim());
+					cmt_status = new Integer(req.getParameter("cmt_status").trim());
 				} catch (NumberFormatException e) {
-					loc_status = 1;
-					errorMsgs.add("地點狀態請填數字.");
+					cmt_status = 1;
+					errorMsgs.add("留言狀態請填數字.");
 				}
+				String rcd_no = req.getParameter("rcd_no").trim();
+				String mb_id = req.getParameter("mb_id").trim();
 
-				String loc_address = req.getParameter("loc_address").trim();
-				if (loc_address == null || loc_address.trim().length() == 0) {
-					errorMsgs.add("地址請勿空白");
-				}
-
-				LocationVO locationVO = new LocationVO();
-//				loc_no, loc_typeno, longitude, latitude, loc_status, loc_address, loc_pic
-				locationVO.setLoc_no(loc_no);
-				locationVO.setLoc_typeno(loc_typeno);
-				locationVO.setLongitude(longitude);
-				locationVO.setLatitude(latitude);
-				locationVO.setLoc_status(loc_status);
-				locationVO.setLoc_address(loc_address);
-				byte[] loc_pic = null;
-//				loc_pic = req.getParameter("loc_pic").getBytes();
-				InputStream in;
-				BufferedInputStream bf;
-
-				try {
-					Part part = req.getPart("loc_pic");
-					in = part.getInputStream();
-//					in = new FileInputStream("/fake_picture/loc" + String.format("%05d", 6) + ".jpg");
-					bf = new BufferedInputStream(in);
-					loc_pic = new byte[bf.available()];// 讀入的圖檔,暫存在記憶體
-					if (loc_pic == null || loc_pic.length == 0) {
-						LocationService src = new LocationService();
-						LocationVO locationVO_old = src.getOneLocation(loc_no);
-						loc_pic = locationVO_old.getLoc_pic();
-					}
-					bf.read(loc_pic);
-					locationVO.setLoc_pic(loc_pic);
-					bf.close();
-					in.close();
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				CmtVO cmtVO = new CmtVO();
+				// cmt_no, cmt_content, cmt_time, cmt_status, rcd_no, mb_id
+				cmtVO.setCmt_no(cmt_no);
+				cmtVO.setCmt_content(cmt_content);
+				cmtVO.setCmt_time(cmt_time);
+				cmtVO.setCmt_status(cmt_status);
+				cmtVO.setRcd_no(rcd_no);
+				cmtVO.setMb_id(mb_id);
 
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
-					req.setAttribute("locationVO", locationVO); // 含有輸入格式錯誤的VO物件,也存入req
-					RequestDispatcher failureView = req.getRequestDispatcher("/front_end/location/update_location_input.jsp");
+					req.setAttribute("cmtVO", cmtVO); // 含有輸入格式錯誤的VO物件,也存入req
+					RequestDispatcher failureView = req.getRequestDispatcher("/front_end/cmt/update_cmt_input.jsp");
 					failureView.forward(req, res);
 					return; // 程式中斷
 				}
 
 				/*************************** 2.開始修改資料 *****************************************/
-				LocationService locationSvc = new LocationService();
-				locationVO = locationSvc.updateLocation(loc_no, loc_typeno, longitude, latitude, loc_status,loc_address, loc_pic);
+				CmtService cmtSvc = new CmtService();
+				cmtVO = cmtSvc.updateCmt(cmt_content, cmt_status, cmt_no);
 
 				/*************************** 3.修改完成,準備轉交(Send the Success view) *************/
-				req.setAttribute("locationVO", locationVO); // 資料庫update成功後,正確的的VO物件,存入req
-				String url = "/front_end/location/listOneLocation.jsp";
+				req.setAttribute("cmtVO", cmtVO); // 資料庫update成功後,正確的的VO物件,存入req
+				String url = "/front_end/cmt/listOneCmt.jsp";
 				RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交listOneEmp.jsp
 				successView.forward(req, res);
 
 				/*************************** 其他可能的錯誤處理 *************************************/
 			} catch (Exception e) {
 				errorMsgs.add("修改資料失敗:" + e.getMessage());
-				RequestDispatcher failureView = req.getRequestDispatcher("/front_end/location/update_location_input.jsp");
+				RequestDispatcher failureView = req.getRequestDispatcher("/front_end/cmt/update_cmt_input.jsp");
 				failureView.forward(req, res);
 			}
 		}
@@ -236,89 +193,54 @@ public class LocationServlet extends HttpServlet {
 
 			try {
 				/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
-				String loc_typeno = req.getParameter("loc_typeno");
+				/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
+				// cmt_no, cmt_content, cmt_time, cmt_status, rcd_no, mb_id
+//				String loc_no = new String(req.getParameter("loc_no").trim());
+//				String cmt_no =req.getParameter("cmt_no");
+				String cmt_content = req.getParameter("cmt_content");
 //				String enameReg = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{2,10}$";
-				if (loc_typeno == null || loc_typeno.trim().length() == 0) {
-					errorMsgs.add("地點編號: 請勿空白");
+				if (cmt_content == null || cmt_content.trim().length() == 0) {
+					errorMsgs.add("留言內容: 請勿空白");
 //				} else if(!ename.trim().matches(enameReg)) { //以下練習正則(規)表示式(regular-expression)
 //					errorMsgs.add("員工姓名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間");
 				}
-
-				String longitude = req.getParameter("longitude").trim();
-				if (longitude == null || longitude.trim().length() == 0) {
-					errorMsgs.add("經度請勿空白");
-				}
-
-				String latitude = req.getParameter("latitude").trim();
-				if (latitude == null || latitude.trim().length() == 0) {
-					errorMsgs.add("緯度請勿空白");
-				}
-
-//				java.sql.Date hiredate = null;
+				java.sql.Date cmt_time = null;
 //				try {
-//					hiredate = java.sql.Date.valueOf(req.getParameter("hiredate").trim());
+//				cmt_time = java.sql.Date.valueOf(req.getParameter("cmt_time").trim());
 //				} catch (IllegalArgumentException e) {
-//					hiredate=new java.sql.Date(System.currentTimeMillis());
+				cmt_time=new java.sql.Date(System.currentTimeMillis());
 //					errorMsgs.add("請輸入日期!");
 //				}
+//				Integer cmt_status = null;
+//				try {
+//					cmt_status = new Integer(req.getParameter("cmt_status").trim());
+//				} catch (NumberFormatException e) {
+//					cmt_status = 1;
+//					errorMsgs.add("留言狀態請填數字.");
+//				}
+				String rcd_no = req.getParameter("rcd_no").trim();
+				String mb_id = req.getParameter("mb_id").trim();
 
-				Integer loc_status = null;
-				try {
-					loc_status = new Integer(req.getParameter("loc_status").trim());
-				} catch (NumberFormatException e) {
-					loc_status = 1;
-					errorMsgs.add("地點狀態請填數字.");
-				}
-
-				String loc_address = req.getParameter("loc_address").trim();
-				if (loc_address == null || loc_address.trim().length() == 0) {
-					errorMsgs.add("地址請勿空白");
-				}
-
-				LocationVO locationVO = new LocationVO();
-//				loc_no, loc_typeno, longitude, latitude, loc_status, loc_address, loc_pic
-				locationVO.setLoc_typeno(loc_typeno);
-				locationVO.setLongitude(longitude);
-				locationVO.setLatitude(latitude);
-				locationVO.setLoc_status(loc_status);
-				locationVO.setLoc_address(loc_address);
-				byte[] loc_pic = null;
-//				loc_pic = req.getParameter("loc_pic").getBytes();
-				InputStream in;
-				BufferedInputStream bf;
-
-				try {
-					Part part = req.getPart("loc_pic");
-					in = part.getInputStream();
-//					in = new FileInputStream("/fake_picture/loc" + String.format("%05d", 6) + ".jpg");
-					bf = new BufferedInputStream(in);
-					loc_pic = new byte[bf.available()];// 讀入的圖檔,暫存在記憶體
-					if (loc_pic == null || loc_pic.length == 0) {
-						errorMsgs.add("地標圖片請勿空白");
-					}
-					bf.read(loc_pic);
-					locationVO.setLoc_pic(loc_pic);
-					bf.close();
-					in.close();
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				CmtVO cmtVO = new CmtVO();
+				// cmt_no, cmt_content, cmt_time, cmt_status, rcd_no, mb_id
+//				cmtVO.setCmt_no(cmt_no);
+				cmtVO.setCmt_content(cmt_content);
+				cmtVO.setCmt_time(cmt_time);
+//				cmtVO.setCmt_status(cmt_status);
+				cmtVO.setRcd_no(rcd_no);
+				cmtVO.setMb_id(mb_id);
 
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
-					req.setAttribute("locationVO", locationVO); // 含有輸入格式錯誤的VO物件,也存入req
-					RequestDispatcher failureView = req.getRequestDispatcher("/front_end/location/addLocation.jsp");
+					req.setAttribute("cmtVO", cmtVO); // 含有輸入格式錯誤的VO物件,也存入req
+					RequestDispatcher failureView = req.getRequestDispatcher("/front_end/cmt/update_cmt_input.jsp");
 					failureView.forward(req, res);
-					return;
+					return; // 程式中斷
 				}
 
 				/*************************** 2.開始新增資料 ***************************************/
-				LocationService locationSvc = new LocationService();
-				locationVO = locationSvc.addLocation(loc_typeno, longitude, latitude, loc_status, loc_address, loc_pic);
+				CmtService cmtSvc = new CmtService();
+				cmtVO = cmtSvc.addCmt(cmt_content, cmt_time, rcd_no, mb_id);
 
 				/*************************** 3.新增完成,準備轉交(Send the Success view) ***********/
 				String url = "/front_end/location/listAllLocation.jsp";
@@ -332,33 +254,33 @@ public class LocationServlet extends HttpServlet {
 				failureView.forward(req, res);
 			}
 		}
-
-		if ("delete".equals(action)) { // 來自listAllEmp.jsp
-
-			List<String> errorMsgs = new LinkedList<String>();
-			// Store this set in the request scope, in case we need to
-			// send the ErrorPage view.
-			req.setAttribute("errorMsgs", errorMsgs);
-
-			try {
-				/*************************** 1.接收請求參數 ***************************************/
-				String loc_no = new String(req.getParameter("loc_no"));
-
-				/*************************** 2.開始刪除資料 ***************************************/
-				LocationService locationSvc = new LocationService();
-				locationSvc.deleteLocation(loc_no);
-
-				/*************************** 3.刪除完成,準備轉交(Send the Success view) ***********/
-				String url = "/front_end/location/listAllLocation.jsp";
-				RequestDispatcher successView = req.getRequestDispatcher(url);// 刪除成功後,轉交回送出刪除的來源網頁
-				successView.forward(req, res);
-
-				/*************************** 其他可能的錯誤處理 **********************************/
-			} catch (Exception e) {
-				errorMsgs.add("刪除資料失敗:" + e.getMessage());
-				RequestDispatcher failureView = req.getRequestDispatcher("/front_end/location/listAllLocation.jsp");
-				failureView.forward(req, res);
-			}
-		}
+//		應該不會有delete
+//		if ("delete".equals(action)) { // 來自listAllEmp.jsp
+//
+//			List<String> errorMsgs = new LinkedList<String>();
+//			// Store this set in the request scope, in case we need to
+//			// send the ErrorPage view.
+//			req.setAttribute("errorMsgs", errorMsgs);
+//
+//			try {
+//				/*************************** 1.接收請求參數 ***************************************/
+//				String loc_no = new String(req.getParameter("loc_no"));
+//
+//				/*************************** 2.開始刪除資料 ***************************************/
+//				LocationService locationSvc = new LocationService();
+//				locationSvc.deleteLocation(loc_no);
+//
+//				/*************************** 3.刪除完成,準備轉交(Send the Success view) ***********/
+//				String url = "/front_end/location/listAllLocation.jsp";
+//				RequestDispatcher successView = req.getRequestDispatcher(url);// 刪除成功後,轉交回送出刪除的來源網頁
+//				successView.forward(req, res);
+//
+//				/*************************** 其他可能的錯誤處理 **********************************/
+//			} catch (Exception e) {
+//				errorMsgs.add("刪除資料失敗:" + e.getMessage());
+//				RequestDispatcher failureView = req.getRequestDispatcher("/front_end/location/listAllLocation.jsp");
+//				failureView.forward(req, res);
+//			}
+//		}
 	}
 }
