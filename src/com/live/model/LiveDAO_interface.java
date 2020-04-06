819bdea95864b9bd21e64b9174ae967e00a3575a
world <world@LAPTOP-VPGLLM1L>
2020-04-06 10:53:23 +0800

    新增LiveDAO_interface BY以文

--- src/com/live/model/LiveDAO_interface.java
+++ src/com/live/model/LiveDAO_interface.java
@@ -1,5 +1,15 @@
 package com.live.model;
 
+import java.util.List;
+
 public interface LiveDAO_interface {
+	
+	public void insert(LiveVO memberVO);
+	public void update(LiveVO memberVO);
+	public void delete(LiveVO memberVO);
+	public LiveVO findByPrimaryKey(String mb_id);
+	public List<LiveVO> getAll();
+	// 萬用複合查詢(傳入參數型態Map)(回傳 List)
+	// public List<LiveVO> getAll(Map<String, String[]> map);
 
 }
