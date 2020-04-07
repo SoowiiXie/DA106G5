39a5370efcd7ccff90caf96954b5863d310e2c33
world <world@LAPTOP-VPGLLM1L>
2020-04-07 21:25:53 +0800

    Live DAO 完成 BY 以文

--- src/com/live/model/LiveDAO_interface.java
+++ src/com/live/model/LiveDAO_interface.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public interface LiveDAO_interface {
 	
-	public void insert(LiveVO memberVO);
-	public void update(LiveVO memberVO);
-	public void delete(LiveVO memberVO);
-	public List<LiveVO> findByPrimaryKey(String mb_id);
+	public void insert(LiveVO liveVO);
+	public void update(LiveVO liveVO);
+	public void delete(String live_no);
+	public List<LiveVO> findByMbId(String mb_id);
 	public List<LiveVO> getAll();
 	// 萬用複合查詢(傳入參數型態Map)(回傳 List)
 	// public List<LiveVO> getAll(Map<String, String[]> map);
