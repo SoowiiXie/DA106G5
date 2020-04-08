89d01c89d9105627bc77a1e7b5aa8a8dd6de1fea
soowii <e64009117@gs.ncku.edu.tw>
2020-04-09 03:14:52 +0800

    Thumb的M及C完成，預計V會全部都在cmt的listAll裡呈現 by戍乂

--- src/com/thumb/model/Thumb_interface.java
+++ src/com/thumb/model/Thumb_interface.java
@@ -1,11 +1,11 @@
 package com.thumb.model;
 
-import java.util.List;
-
 public interface Thumb_interface {
 	 public void insert(ThumbVO thumbVO);
-     public void update(ThumbVO thumbVO);
+//     public void update(ThumbVO thumbVO);
      public void delete(String rcd_no, String mb_id);
-     public ThumbVO findByPrimaryKey(String rcd_no, String mb_id);
-     public List<ThumbVO> getAll();
+//     public ThumbVO findByPrimaryKey(String rcd_no, String mb_id);
+//     public List<ThumbVO> getAll();
+	 // 萬用型
+	 public Integer countAllThumb(String rcd_no);
 }
