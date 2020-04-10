9d0ffae2b0022230cae1f8fa1fd0f666247680b2
soowii <e64009117@gs.ncku.edu.tw>
2020-04-11 05:27:41 +0800

    註冊loc_rpt的web.xml by戍乂

--- src/com/loc_rpt/model/Loc_rptTest.java
+++ src/com/loc_rpt/model/Loc_rptTest.java
@@ -15,42 +15,42 @@ public class Loc_rptTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		Cmt_rptDAO dao = new Cmt_rptDAO();
+		Loc_rptDAO dao = new Loc_rptDAO();
 
 		 //新增
-		Cmt_rptVO cmt_rptVO1 = new Cmt_rptVO();
+		Loc_rptVO cmt_rptVO1 = new Loc_rptVO();
 		cmt_rptVO1.setRpt_reason("這個我不行!!!!!!");
 		cmt_rptVO1.setRpt_status(1);
-		cmt_rptVO1.setCmt_no("cmt00001");
+		cmt_rptVO1.setLoc_no("loc00001");
 		cmt_rptVO1.setMb_id("soowii123");
 		dao.insert(cmt_rptVO1);
 
 		//修改
-		Cmt_rptVO cmt_rptVO2 = new Cmt_rptVO();
+		Loc_rptVO cmt_rptVO2 = new Loc_rptVO();
 		cmt_rptVO2.setRpt_reason("他我可以");
 		cmt_rptVO2.setRpt_status(1);
-		cmt_rptVO2.setCmt_rpt_no("cmtr00006");
+		cmt_rptVO2.setLoc_rpt_no("locr00006");
 		dao.update(cmt_rptVO2);
 
 		//刪除
 		dao.delete("cmtr00006");
 
 		//查詢
-		Cmt_rptVO cmt_rptVO3 = dao.findByPrimaryKey("cmtr00001");
-		System.out.print(cmt_rptVO3.getCmt_rpt_no() + ",");
+		Loc_rptVO cmt_rptVO3 = dao.findByPrimaryKey("cmtr00001");
+		System.out.print(cmt_rptVO3.getLoc_rpt_no() + ",");
 		System.out.print(cmt_rptVO3.getRpt_reason() + ",");
 		System.out.print(cmt_rptVO3.getRpt_status() + ",");
-		System.out.print(cmt_rptVO3.getCmt_no() + ",");
+		System.out.print(cmt_rptVO3.getLoc_no() + ",");
 		System.out.print(cmt_rptVO3.getMb_id() + "\n");
 		System.out.println("---------------------");
 
 		// 查詢getall
-		List<Cmt_rptVO> list = dao.getAll();
-		for (Cmt_rptVO cmt_rptVO : list) {
-			System.out.print(cmt_rptVO.getCmt_rpt_no() + ",");
+		List<Loc_rptVO> list = dao.getAll();
+		for (Loc_rptVO cmt_rptVO : list) {
+			System.out.print(cmt_rptVO.getLoc_rpt_no() + ",");
 			System.out.print(cmt_rptVO.getRpt_reason() + ",");
 			System.out.print(cmt_rptVO.getRpt_status() + ",");
-			System.out.print(cmt_rptVO.getCmt_no() + ",");
+			System.out.print(cmt_rptVO.getLoc_no() + ",");
 			System.out.print(cmt_rptVO.getMb_id());
 
 			System.out.println();
