19f8069caed6ef836413043101cd65cc9a469ee1
world <world@LAPTOP-VPGLLM1L>
2020-04-12 19:45:51 +0800

    LIVE_RPT DAO更新 BY 以文

--- src/com/live_rpt/model/Live_rptDAO_interface.java
+++ src/com/live_rpt/model/Live_rptDAO_interface.java
@@ -1,6 +1,27 @@
 package com.live_rpt.model;
 
+import java.util.List;
+import java.util.Map;
+
+
 public interface Live_rptDAO_interface {
+	
+	public void insert(Live_rptVO Live_rptVO);
+
+	public void update(Live_rptVO Live_rptVO);
+
+	public void updateByCmtNo(Live_rptVO Live_rptVO);
+	
+	public String getRptedMb_id(String live_rpt_no);
+
+	public void delete(String live_rpt_no);
+
+	public Live_rptVO findByPrimaryKey(String live_rpt_no);
+
+	public List<Live_rptVO> getAll();
+
+	// 萬用型
+	public List<Live_rptVO> getAllUWish(Map<String, String[]> map);
 	
 	
 
