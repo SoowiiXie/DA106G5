3eb54a5c95172c96cce86bf742f0ca36a5d2e26e
world <world@LAPTOP-VPGLLM1L>
2020-04-12 17:22:37 +0800

    更新 Mb_follow model BY 以文

---
+++ src/com/mb_follow/model/Mb_followDAO.java
@@ -0,0 +1,31 @@
+package com.mb_follow.model;
+
+import java.util.List;
+
+public class Mb_followDAO implements Mb_followDAO_interface{
+
+	@Override
+	public void insert(String mb_id, String md_id_followed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(String mb_id, String md_id_followed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Mb_followVO> findByMbId(String mb_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Mb_followVO> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/mb_follow/model/Mb_followDAO_interface.java
@@ -0,0 +1,13 @@
+package com.mb_follow.model;
+
+import java.util.List;
+
+public interface Mb_followDAO_interface {
+	public void insert(String mb_id,String md_id_followed);
+	public void delete(String mb_id,String md_id_followed);
+	public List<Mb_followVO> findByMbId(String mb_id);
+	public List<Mb_followVO> getAll();
+	// 萬用複合查詢(傳入參數型態Map)(回傳 List)
+	// public List<LiveVO> getAll(Map<String, String[]> map);
+
+}
