23743ffe5947372bb069fb2ec84a1f642c08caaa
soowii <e64009117@gs.ncku.edu.tw>
2020-04-13 15:29:00 +0800

    合併名軒的假圖進FakeBlob.java裡，順便放入兩個假會員圖 by戍乂

--- src/com/common/FakeBlob.java
+++ src/com/common/FakeBlob.java
@@ -6,9 +6,11 @@ import static com.common.Common.URL;
 import static com.common.Common.USER;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -92,6 +94,66 @@ class FakeBlob {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		
+		// 8個會員
+		String[] mb_id = {"soowii123","xuan123","michael123","vain123","yiwen123","weijhih123","androidlababy520","anjavababy520"};
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		InputStream fin = null;
+		int count = 0;
+
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "DA106G5", "DA106G5");
+			pstmt = con.prepareStatement("UPDATE MEMBER SET MB_PIC=? WHERE MB_ID = ?");
+			// 6張照片
+			for(int i = 0; i < 8; i++) {
+				
+				fin = new FileInputStream(new File("WebContent/fake_picture", "mb" + (i+1) + ".jpg")); 
+				
+				pstmt.setBinaryStream(1, fin, fin.available());
+				pstmt.setString(2, mb_id[i]);
+				count += pstmt.executeUpdate();
+				
+			}
+			System.out.println(count + " images inserted!!");
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+			// Clean up JDBC resources
+		} finally {
+			if(fin != null) {
+				try {
+					fin.close();
+				} catch (IOException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
 		}
 	}
 }
