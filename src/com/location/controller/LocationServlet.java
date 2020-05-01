7004dd04a66e95885add6279e8047b748e0a3946
soowii <e64009117@gs.ncku.edu.tw>
2020-05-02 02:37:26 +0800

    地標檢舉和地標審核的被include畫面完成 by戍乂

--- src/com/location/controller/LocationServlet.java
+++ src/com/location/controller/LocationServlet.java
@@ -358,5 +358,39 @@ public class LocationServlet extends HttpServlet {
 				failureView.forward(req, res);
 			}
 		}
+		
+//		fakeDelete=上/下架，因為不會刪除也等於only update cmt_status
+		if ("fakeDelete".equals(action)) { // 來自listAllEmp.jsp
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+			try {
+				/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
+				// cmt_rpt_no, rpt_reason, rpt_status, cmt_no, mb_id
+//				String loc_no = new String(req.getParameter("loc_no").trim());
+				String loc_no = req.getParameter("loc_no").trim();
+				Integer loc_status = new Integer(req.getParameter("loc_status").trim());
+				if (loc_status==1) {//如果是上架(1)，就改成下架(2)
+					loc_status=2;
+				}else {//剩下的情況就是下架(2)，改成上架(1)
+					loc_status=1;
+				}
+				/*************************** 2.開始修改資料 *****************************************/
+				LocationService locationSvc = new LocationService();
+				LocationVO locationVO = locationSvc.getOneLocation(loc_no);
+				locationSvc.updateLocation(loc_no, locationVO.getLoc_typeno(), locationVO.getLongitude(), locationVO.getLatitude(), loc_status, locationVO.getLoc_address(), locationVO.getLoc_pic());
+				/*************************** 3.修改完成,準備轉交(Send the Success view) *************/
+				String url ="/front_end/location/listAllLocation.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交listOneEmp.jsp
+				successView.forward(req, res);
+
+				/*************************** 其他可能的錯誤處理 *************************************/
+			} catch (Exception e) {
+				errorMsgs.add("修改資料失敗:" + e.getMessage());
+				RequestDispatcher failureView = req.getRequestDispatcher("/front_end/location/listAllLocation.jsp");
+				failureView.forward(req, res);
+			}
+		}
 	}
 }
