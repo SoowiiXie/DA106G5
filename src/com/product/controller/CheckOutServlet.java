243507464997c91e19f1da98afafe74e68712635
soowii <e64009117@gs.ncku.edu.tw>
2020-05-10 14:36:32 +0800

    更改成功路徑  by戍乂

--- src/com/product/controller/CheckOutServlet.java
+++ src/com/product/controller/CheckOutServlet.java
@@ -158,7 +158,7 @@ public class CheckOutServlet extends HttpServlet {
 			session.setAttribute("buylistCount", 0);
 			
 			//跳轉網址(4,5)
-			String url = "https://da106g5.tk/DA106_G5/front_end/product/MemberTransaction_record.jsp";
+			String url = "https://www.da106g5.tk/DA106_G5/front_end/product/ShopHome.jsp";
 //			od_detailLineVO.setConfirmUrl(url);
 			od_detailLineVO.setConfirmUrl(url);
 			
